a5ed7ad1f5f9c240743a5e9fd5fd13097c74f773
sarayu2008 <sarayu.yerrapragada@gmail.com>
2025-02-24 23:00:49 +0530

    Add files via upload

---
+++ test12.py
@@ -0,0 +1,15 @@
+
+def convert_mp3_to_webm(input_file, bitrate="128k"):
+    output_file = input_file.split(".")[0]+".webm"
+    print(input_file)
+    print(output_file)
+    cmd = f"ffmpeg -i {input_file} {output_file} -y"
+    print(cmd)
+    os.system(cmd)
+    os.remove(input_file)
+def text_to_speech(ai_generated_response):
+    # Convert AI response to speech using gTTS
+    tts = gTTS(text=ai_generated_response, lang='en')
+    tts.save("response.mp3")  # Save the speech audio as a file
+    convert_mp3_to_webm("response.mp3")
+text_to_speech()
